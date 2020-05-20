3b5bb45951a3f317d4c6a714696b37e85aed9cb8
Ricardo Branco <exodon@gmail.com>
2020-05-20 12:18:33 +0200

    - First Commit with default team

---
+++ README.md
@@ -0,0 +1,65 @@
+<h1>October CMS Gulp Starter</h1>
+
+<p>Lightweight optimized Gulp starter for October CMS theme development with Gulp 4, Browsersync, preprocessors (sass, scss, less, stylus), cleancss, uglify-es, autoprefixer, rsync and more.</p>
+
+<p>
+	<img src="https://raw.githubusercontent.com/agragregra/agragregra.github.com/master/images/october-gulp-preview.jpg" alt="Optober Gulp">
+</p>
+
+<h2>Installation</h2>
+
+<p>Run this in the root directory of you site <strong>with a dot</strong> at the end:</p>
+
+<code>git clone https://github.com/agragregra/OctoberCMS-Gulp-Starter .</code>
+
+<h2>October CMS Theme Directory structure</h2>
+
+<p>Recommends using the following base directory structure for you theme:</p>
+
+<pre>
+themes/
+— mytheme/           <= Theme starts here
+— — pages/             <= Pages directory
+— — — home.htm
+— — layouts/           <= Layouts directory
+— — — default.htm
+— — partials/          <= Partials directory (Optional)
+— — — header.htm
+— — — footer.htm
+— — content/           <= Content directory
+— — — intro.htm
+— — assets/            <= Assets directory
+— — — css/
+— — — — theme.min.css  <= Minified styles
+— — — js/
+— — — — app.js         <= Main user JavaScript file
+— — — — theme.min.js   <= Minified scripts
+— — — images/
+— — — sass/            <= Preprocessor folder (Maybe sass, scss, less, styl)
+— — — — theme.sass     <= Main user Sass file (Maybe theme.sass, theme.scss, theme.less or theme.styl)
+— — — vendor/          <= Vendor scripts or styles (For example - jQuery, Bootstrap, etc..)
+— — theme.yaml
+</pre>
+
+<h2>Starter Features & Variables in gulpfile.js</h2>
+
+<ol>
+	<li><strong>Make proxy for live development with Browsersync</strong> - <code>let localhost</code></li>
+	<li><strong>Simple preprocessor selection (sass, scss, less, styl)</strong> - <code>let preprocessor</code></li>
+	<li><strong>Simple selection of the current theme</strong> - <code>let theme</code></li>
+	<li><strong>Simple selection of file extensions to watch & reload</strong> - <code>let fileswatch</code></li>
+	<li><strong>Better CSS</strong> - <code>gulp-clean-css, gulp-autoprefixer</code>. Importing framework extras CSS into theme preprocessor file</li>
+	<li><strong>Better JS</strong> - <code>gulp-uglify-es</code>. Vendor scripts & framework extras plug into scripts functions.</li>
+	<li><strong>Better deploy with rsync</strong> - <code>gulp-rsync</code></li>
+</ol>
+
+<h2>Exported Gulp Task</h2>
+
+<ul>
+	<li><strong>browsersync</strong></li>
+	<li><strong>assets</strong> - Compile assets (CSS & JS)</li>
+	<li><strong>styles</strong> - Compile and concat CSS with selected preprocessor</li>
+	<li><strong>scripts</strong> - Compile, minify and concat JS with gulp-uglify-es & gulp-concat</li>
+	<li><strong>deploy</strong> - deploy changes on server with gulp-rsync</li>
+	<li><strong>gulp</strong> - The default command to start the environment</li>
+</ul>
---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+// VARIABLES & PATHS
+
+let localhost    = 'http://localhost:88/experiment', // Local domain
+    preprocessor = 'sass', // Preprocessor (sass, scss, less, styl) / Preprocessor folder name / Module require const name. Example: themes/mytheme/assets/scss/
+    theme        = 'mytheme', // Theme folder name
+    fileswatch   = 'html,htm,php,txt,yaml,twig,json,md', // List of files extensions for watching & hard reload (comma separated)
+    online       = true; // If «false» - Browsersync will work offline without internet connection
+
+let paths = {
+
+	scripts: {
+		src: [
+			// 'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
+			// 'themes/' + theme + '/assets/vendor/lazyload/lazyload.js', // Vendor script plugin example
+			// 'modules/system/assets/js/framework.js', // {% framework extras %}
+			// 'modules/system/assets/js/framework.extras.js', // {% framework extras %}
+			// 'plugins/nms/plugin/assets/js/plugin.js', // Plugin script example
+			'themes/' + theme + '/assets/js/app.js' // Theme app.js. Always at the end
+		],
+	},
+
+	deploy: {
+		hostname:    'username@yousite.com', // Deploy hostname
+		destination: 'yousite/public_html/', // Deploy destination
+		include:     [ '*.htaccess', ], // Included files to deploy
+		exclude:     [ // Excluded files from deploy
+			'storage/*.sqlite',
+			'**/Thumbs.db',
+			'**/*.DS_Store',
+			'node_modules',
+			'gulpfile.js',
+			'package.json',
+			'package-lock.json',
+			'npm-debug.log',
+			'themes/' + theme + '/assets/js/app.js',
+			'themes/' + theme + '/assets/' + preprocessor,
+		],
+	},
+
+}
+
+// LOGIC
+
+const { src, dest, parallel, series, watch } = require('gulp');
+const sass         = require('gulp-sass');
+const scss         = require('gulp-sass');
+const less         = require('gulp-less');
+const styl         = require('gulp-stylus');
+const cleancss     = require('gulp-clean-css');
+const concat       = require('gulp-concat');
+const browserSync  = require('browser-sync').create();
+const uglify       = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const rsync        = require('gulp-rsync');
+
+function browsersync() {
+	browserSync.init({
+		proxy: localhost,
+		notify: false,
+		online: online
+	})
+}
+
+function scripts() {
+	return src(paths.scripts.src)
+	.pipe(concat('theme.min.js'))
+	.pipe(uglify())
+	.pipe(dest('themes/' + theme + '/assets/js'))
+	.pipe(browserSync.stream())
+}
+
+function styles() {
+	return src('themes/' + theme + '/assets/' + preprocessor + '/main.*')
+	.pipe(eval(preprocessor)())
+	.pipe(concat('theme.min.css'))
+	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } },/* format: 'beautify' */ }))
+	.pipe(dest('themes/' + theme + '/assets/css'))
+	.pipe(browserSync.stream())
+}
+
+function deploy() {
+	return src('/')
+	.pipe(rsync({
+		root: '/',
+		hostname: paths.deploy.hostname,
+		destination: paths.deploy.destination,
+		include: paths.deploy.include,
+		exclude: paths.deploy.exclude,
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+}
+
+function startwatch() {
+	watch('themes/' + theme + '/assets/' + preprocessor + '/**/*', styles);
+	watch(['themes/'  + theme + '/assets/js/**/*.js', '!themes/' + theme + '/assets/js/*.min.js', 'themes/'  + theme + '/assets/vendor/**/*.js'], scripts);
+	watch(['themes/' + theme + '/**/*.{' + fileswatch + '}', 'plugins/**/*.{' + fileswatch + '}']).on('change', browserSync.reload);
+}
+
+exports.browsersync = browsersync;
+exports.assets      = parallel(styles, scripts);
+exports.styles      = styles;
+exports.scripts     = scripts;
+exports.deploy      = deploy;
+exports.default     = parallel(styles, scripts, browsersync, startwatch);
