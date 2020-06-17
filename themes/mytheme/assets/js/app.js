bfdfc6bf9604365755e44053939c1e931cd29cf5
Ricardo Branco <exodon@gmail.com>
2020-06-17 14:42:00 +0200

    Add hamburguer menu js

--- themes/mytheme/assets/js/app.js
+++ themes/mytheme/assets/js/app.js
@@ -12,4 +12,16 @@ document.addEventListener("DOMContentLoaded", function() {
 		},
 	});
 
+
+/**
+* @function
+* @description Hamburger Menu
+*/
+	$(".navbar-burger").click(function () {
+
+		// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+		$(".navbar-burger").toggleClass("is-active");
+		$(".navbar-menu").toggleClass("is-active");
+	});
+
 });
