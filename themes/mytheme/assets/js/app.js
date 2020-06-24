81cdb087ba77307ffe4750382b8efe0d0601c79d
Ricardo Branco <exodon@gmail.com>
2020-06-24 16:12:57 +0200

    -add faq page -improvements on author

--- themes/mytheme/assets/js/app.js
+++ themes/mytheme/assets/js/app.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-/**
- * @function
- * @description Swiper Home Page
- */
+	/**
+	 * @function
+	 * @description Swiper Home Page
+	 */
 	var swiper = new Swiper('.swiper-container', {
 		slidesPerView: 1,
 		pagination: {
@@ -12,16 +12,28 @@ document.addEventListener("DOMContentLoaded", function() {
 		},
 	});
 
-
-/**
-* @function
-* @description Hamburger Menu
-*/
+	/**
+	* @function
+	* @description Hamburger Menu
+	*/
 	$(".navbar-burger").click(function () {
 
 		// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
 		$(".navbar-burger").toggleClass("is-active");
 		$(".navbar-menu").toggleClass("is-active");
+	});
+
+	/**
+	* @function
+	* @description Accordion
+	*/
+	$('.accordion__header').click(function () {
+
+		$(".accordion__body").not($(this).next()).slideUp(400);
+		$(this).next().slideToggle(400);
+
+		$(".accordion__item").not($(this).closest(".accordion__item")).removeClass("open-accordion");
+		$(this).closest(".accordion__item").toggleClass("open-accordion");
 	});
 
 });
