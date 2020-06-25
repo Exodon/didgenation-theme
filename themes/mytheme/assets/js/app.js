7a9bd2be43361e8116e2f291e11d6fd42dee230c
Ricardo Branco <exodon@gmail.com>
2020-06-25 11:47:53 +0200

    -clean js -fix poll problem - add scripts to the scritps.html file

--- themes/mytheme/assets/js/app.js
+++ themes/mytheme/assets/js/app.js
@@ -16,11 +16,11 @@ document.addEventListener("DOMContentLoaded", function() {
 	* @function
 	* @description Hamburger Menu
 	*/
-	$(".navbar-burger").click(function () {
+	$('.navbar-burger').click(function () {
 
-		// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-		$(".navbar-burger").toggleClass("is-active");
-		$(".navbar-menu").toggleClass("is-active");
+		// Toggle the 'is-active' class on both the 'navbar-burger' and the 'navbar-menu'
+		$('.navbar-burger').toggleClass('is-active');
+		$('.navbar-menu').toggleClass('is-active');
 	});
 
 	/**
@@ -29,11 +29,11 @@ document.addEventListener("DOMContentLoaded", function() {
 	*/
 	$('.accordion__header').click(function () {
 
-		$(".accordion__body").not($(this).next()).slideUp(400);
+		$('.accordion__body').not($(this).next()).slideUp(400);
 		$(this).next().slideToggle(400);
 
-		$(".accordion__item").not($(this).closest(".accordion__item")).removeClass("open-accordion");
-		$(this).closest(".accordion__item").toggleClass("open-accordion");
+		$('.accordion__item').not($(this).closest('.accordion__item')).removeClass('open-accordion');
+		$(this).closest('.accordion__item').toggleClass('open-accordion');
 	});
 
 });
